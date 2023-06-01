6c5514d61c7601d4f37ddf3d4456f12c7a81f792
ArtTheFirst <roberts.protege277@gmail.com>
2023-06-01 13:29:00 +0100

    do the putchar thing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "artthefirst.h"
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	char *h = "artthefirst\n";
+	char *h = "main\n";
 
 	while (*h)
 	{
